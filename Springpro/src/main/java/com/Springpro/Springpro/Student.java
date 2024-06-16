879676da2e78dda9f4bad7253e08510543fc11da
mkirwa <mahlonkibiwott@gmail.com>
2024-06-16 12:25:59 -0600

    Updates

---
+++ Springpro/src/main/java/com/Springpro/Springpro/Student.java
@@ -0,0 +1,24 @@
+package com.Springpro.Springpro;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="STUDENT")
+
+public class Student {
+
+    @Id
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "MARK")
+    private int mark;
+
+    @Column(name = "NAME")
+    private String name;
+}
---
+++ cafe/src/main/java/com/inn/cafe/rest/UserRest.java
@@ -0,0 +1,16 @@
+package com.inn.cafe.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+// For an endpoint. To be used to connect to the api
+@RequestMapping(path= "/user")
+public interface UserRest {
+    // Defining our API
+    @PostMapping(path="/signup")
+    public ResponseEntity<String> signUp(@RequestBody(required = true) Map<String, String> requestMap);
+}
