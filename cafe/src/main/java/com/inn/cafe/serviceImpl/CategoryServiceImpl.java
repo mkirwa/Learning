21b70119b08eb31e79325e1ce204eb63a2d8a3ba
mkirwa <mahlonkibiwott@gmail.com>
2024-07-19 05:56:17 -0600

    changes

--- cafe/src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
+++ cafe/src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
@@ -1,8 +1,40 @@
 package com.inn.cafe.serviceImpl;
 
+import com.inn.cafe.JWT.JwtFilter;
+import com.inn.cafe.constants.CafeConstants;
+import com.inn.cafe.dao.CategoryDao;
 import com.inn.cafe.service.CategoryService;
+import com.inn.cafe.utils.CafeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    CategoryDao categoryDao;
+
+    @Autowired
+    JwtFilter jwtFilter;
+    /**
+     * @param requestMap
+     * @return
+     */
+    @Override
+    public ResponseEntity<String> addNewCategory(Map<String, String> requestMap) {
+        try {
+            if(jwtFilter.isAdmin()){
+
+            } else{
+                return CafeUtils.getResponseEntity(CafeConstants.UNAUTHORIZED_ACCESS, HttpStatus.UNAUTHORIZED);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
