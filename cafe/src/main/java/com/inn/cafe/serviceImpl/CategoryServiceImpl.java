2d654ae484bc8b2128567eb20e0b3b5a477bbc0e
mkirwa <mahlonkibiwott@gmail.com>
2024-07-19 05:18:41 -0600

    changes

---
+++ cafe/src/main/java/com/inn/cafe/serviceImpl/CategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.inn.cafe.serviceImpl;
+
+import com.inn.cafe.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+}
