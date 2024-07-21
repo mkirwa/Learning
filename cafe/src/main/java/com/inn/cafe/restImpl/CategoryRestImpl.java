dfb3b85b9478eddfa7bd3c58d6d05784f9d07192
mkirwa <mahlonkibiwott@gmail.com>
2024-07-20 22:51:26 -0600

    changes

--- cafe/src/main/java/com/inn/cafe/restImpl/CategoryRestImpl.java
+++ cafe/src/main/java/com/inn/cafe/restImpl/CategoryRestImpl.java
@@ -1,5 +1,6 @@
 package com.inn.cafe.restImpl;
 
+import com.inn.cafe.POJO.Category;
 import com.inn.cafe.constants.CafeConstants;
 import com.inn.cafe.rest.CategoryRest;
 import com.inn.cafe.service.CategoryService;
@@ -9,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -24,6 +27,34 @@ public class CategoryRestImpl implements CategoryRest {
     public ResponseEntity<String> addNewCategory(Map<String, String> requestMap) {
         try {
             return categoryService.addNewCategory(requestMap);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * @param filterValue
+     * @return
+     */
+    @Override
+    public ResponseEntity<List<Category>> getAllCategory(String filterValue) {
+        try {
+            return categoryService.getAllCategory(filterValue);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * @param requestMap
+     * @return
+     */
+    @Override
+    public ResponseEntity<String> updateCategory(Map<String, String> requestMap) {
+        try {
+            return categoryService.updateCategory(requestMap);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
