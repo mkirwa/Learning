f3fc835154f7bf114833c8a99f7fb5c2fbdbefcf
mkirwa <mahlonkibiwott@gmail.com>
2024-07-28 00:32:08 -0600

    changes

--- cafe/src/main/java/com/inn/cafe/restImpl/ProductRestImpl.java
+++ cafe/src/main/java/com/inn/cafe/restImpl/ProductRestImpl.java
@@ -59,4 +59,18 @@ public class ProductRestImpl implements ProductRest {
         }
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    /**
+     * @param id
+     * @return
+     */
+    @Override
+    public ResponseEntity<String> deleteProduct(Integer id) {
+        try{
+            return productService.deleteProduct(id);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
