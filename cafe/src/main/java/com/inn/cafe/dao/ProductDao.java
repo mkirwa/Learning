f9762bb5a2ce55bb555aa31552998b24f0eddfad
mkirwa <mahlonkibiwott@gmail.com>
2024-07-28 18:25:07 -0600

    changes

--- cafe/src/main/java/com/inn/cafe/dao/ProductDao.java
+++ cafe/src/main/java/com/inn/cafe/dao/ProductDao.java
@@ -2,11 +2,19 @@ package com.inn.cafe.dao;
 
 import com.inn.cafe.POJO.Product;
 import com.inn.cafe.wrapper.ProductWrapper;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductDao extends JpaRepository<Product, Integer> {
 
     List<ProductWrapper> getAllProduct();
+
+    // You have to add these annotations otherwise, it will not update
+    @Modifying
+    @Transactional
+    Integer updateProductStatus(@Param("status") String status, @Param("id") Integer id);
 }
