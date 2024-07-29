f0a0f9acb8334ce9fbc505e2bf67a304b6078c1b
mkirwa <mahlonkibiwott@gmail.com>
2024-07-29 00:34:53 -0600

    changes

--- cafe/src/main/java/com/inn/cafe/restImpl/BillRestImpl.java
+++ cafe/src/main/java/com/inn/cafe/restImpl/BillRestImpl.java
@@ -1,6 +1,29 @@
 package com.inn.cafe.restImpl;
 
+import com.inn.cafe.constants.CafeConstants;
 import com.inn.cafe.rest.BillRest;
+import com.inn.cafe.service.BillService;
+import com.inn.cafe.utils.CafeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
 
 public class BillRestImpl implements BillRest {
+    @Autowired
+    BillService billService;
+    /**
+     * @param requestMap
+     * @return
+     */
+    @Override
+    public ResponseEntity<String> generateReport(Map<String, Object> requestMap) {
+        try{
+            return billService.generateReport(requestMap);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
