7c9af6f1d8fd622c7835cefbbcce9393defbadd2
mkirwa <mahlonkibiwott@gmail.com>
2024-08-01 11:58:55 -0600

    changes

--- cafe/src/main/java/com/inn/cafe/utils/CafeUtils.java
+++ cafe/src/main/java/com/inn/cafe/utils/CafeUtils.java
@@ -1,9 +1,16 @@
 package com.inn.cafe.utils;
 
+import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CafeUtils {
 
@@ -18,6 +25,21 @@ public class CafeUtils {
         Date date = new Date();
         long time = date.getTime();
         return "BILL-" + time;
+    }
+
+    public static JSONArray getJsonArrayFromString(String data) throws JSONException{
+        JSONArray jsonArray = new JSONArray(data);
+        return jsonArray;
+    }
+
+    // Function to convert JSON Array into the form of map string comma object
+    public static Map<String, Object> getMapFromJson(String data){
+        if(!Strings.isNullOrEmpty(data)){
+            return new Gson().fromJson(data, new TypeToken<Map<String, Object>>(){
+            }.getType());
+        }else{
+            return new HashMap<>();
+        }
     }
 
 }
