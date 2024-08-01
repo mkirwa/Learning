b59fc44cb0bf2795d966044bda8e60c792819a29
mkirwa <mahlonkibiwott@gmail.com>
2024-08-01 11:58:55 -0600

    changes

--- cafe/src/main/java/com/inn/cafe/serviceImpl/BillServiceImpl.java
+++ cafe/src/main/java/com/inn/cafe/serviceImpl/BillServiceImpl.java
@@ -12,6 +12,7 @@ import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -73,6 +74,14 @@ public class BillServiceImpl implements BillService {
                 PdfPTable table = new PdfPTable(5);
                 // Adding header to the tables
                 addTableHeader(table);
+                // Adding data to the table
+                JSONArray jsonArray = CafeUtils.getJsonArrayFromString((String) requestMap.get("productDetails"));
+
+                // Loop to add rows into data tables
+                for(int i = 0; i <jsonArray.length(); i++){
+
+                }
+
             } else {
                 return CafeUtils.getResponseEntity("Required data not found.", HttpStatus.BAD_REQUEST);
             }
@@ -95,6 +104,7 @@ public class BillServiceImpl implements BillService {
                     header.setBackgroundColor(BaseColor.YELLOW);
                     header.setHorizontalAlignment(Element.ALIGN_CENTER);
                     header.setVerticalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(header);
                 });
     }
 
