6acc31851deff639658e1b96dbfae9d84073cb6c
mkirwa <mahlonkibiwott@gmail.com>
2024-08-11 22:47:07 -0600

    changes

--- cafe/Frontend/src/app/services/snackbar.service.ts
+++ cafe/Frontend/src/app/services/snackbar.service.ts
@@ -2,21 +2,29 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 // TODO: Review and add comments -> Created by Mahlon Kirwa
-// Used to display messages 
+// Used to display messages
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SnackbarService {
+  constructor(private snackBar: MatSnackBar) {}
 
-  constructor(private snackBar:MatSnackBar) { }
-
-  openShackBar(message:string, action:string){
-    this.snackBar.open(message, '', {
-      horizontalPosition: 'center',
-      verticalPosition: 'top',
-      duration: 2000,
-      panelClass: ['black-snackbar'] //black-snackbar is a css class defined in styles.css from Frontend/src/styles.css
-    });
+  openShackBar(message: string, action: string) {
+    if (action === 'error') {
+      this.snackBar.open(message, '', {
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+        duration: 2000,
+        panelClass: ['black-snackbar'], //black-snackbar is a css class defined in styles.css from Frontend/src/styles.css
+      });
+    } else {
+      this.snackBar.open(message, action, {
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+        duration: 2000,
+        panelClass: ['green-snackbar'], //green-snackbar is a css class defined in styles.css from Frontend/src/styles.css
+      });
+    }
   }
 }
