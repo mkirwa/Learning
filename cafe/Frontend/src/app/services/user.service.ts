a47f15d300b442c1524dffb85bd24c8e2d6759b4
mkirwa <mahlonkibiwott@gmail.com>
2024-08-11 22:27:03 -0600

    changes

--- cafe/Frontend/src/app/services/user.service.ts
+++ cafe/Frontend/src/app/services/user.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
+// TODO: Review and add comments -> Created by Mahlon Kirwa
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  url = environment.apiUrl
+  constructor(private httpClient:HttpClient) { }
 
-  constructor() { }
+  // Create method for signup
+  signup(data:any){
+    return this.httpClient.post(`${this.url}/user/signup`,data,{
+      headers: new HttpHeaders().set('Content-Type','application/json')
+    })
+  }
 }
