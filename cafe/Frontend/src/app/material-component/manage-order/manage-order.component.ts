c23dae48e62d1bab1d05a5096b0f94a3f8fa73db
mkirwa <mahlonkibiwott@gmail.com>
2025-02-22 23:12:47 -0700

    changes

--- cafe/Frontend/src/app/material-component/manage-order/manage-order.component.ts
+++ cafe/Frontend/src/app/material-component/manage-order/manage-order.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { FormGroup } from '@material-ui/core';
-import { saveAs } from 'file-saver';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import * as saveAs from 'file-saver';
 import { BillService } from 'src/app/services/bill.service';
 import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
@@ -12,38 +12,34 @@ import { GlobalConstants } from 'src/app/shared/global-constants';
 @Component({
   selector: 'app-manage-order',
   templateUrl: './manage-order.component.html',
-  styleUrl: './manage-order.component.scss'
+  styleUrls: ['./manage-order.component.scss']
 })
 export class ManageOrderComponent implements OnInit {
-
-  displayedColumns: string[] = ['name', 'category', 'price', 'quantity','total','edit'];
-  dataSource:any = [];
-  manageOrderForm: any = FormGroup
-  categories: any = [];
+  displayedColumns: string[] = ['name', 'category', 'price', 'quantity', 'total', 'edit'];
+  dataSource: any = [];
+  manageOrderForm: any = FormGroup;
+  categorys: any = [];
   products: any = [];
   price: any;
   totalAmount: number = 0;
   responseMessage: any;
 
-  // constructor is used to initialize the component. Initializing the component helps to set up the component with any necessary dependencies or services.
-  // Without a constructor, the component would not be able to function properly or interact with other parts of the application.
-  constructor(private formBuilder:FormBuilder,
+  constructor(
+    private formBulider: FormBuilder,
     private categoryService: CategoryService,
     private productService: ProductService,
-    private snackbarService: SnackbarService,
     private billService: BillService,
-    private ngxService: NgxUiLoaderService
-  ) { }
+    private dialog: MatDialog,
+    private SnackbarService: SnackbarService,
+    private router: Router) { }
 
-  // ngOnInit is a lifecycle hook that is called after the component has been initialized. It is used to perform any necessary setup or initialization for the component.
-  // In this case, the ngOnInit method is used to start the ngxService loader and get the categories from the categoryService.
   ngOnInit(): void {
-    this.ngxService.start();
-    this.getCategories();
-    this.manageOrderForm = this.formBuilder.group({
+    this.getCategorys();
+    this.manageOrderForm = this.formBulider.group({
       name: [null, [Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
       email: [null, [Validators.required, Validators.pattern(GlobalConstants.emailRegex)]],
-      contactNumber: [null, [Validators.required, Validators.pattern(GlobalConstants.contactNumberRegex)]],
+      contactNumber: [null, [Validators.required]],
+      paymentMethod: [null, [Validators.required]],
       product: [null, [Validators.required]],
       category: [null, [Validators.required]],
       quantity: [null, [Validators.required]],
@@ -52,143 +48,141 @@ export class ManageOrderComponent implements OnInit {
     });
   }
 
-  // getCategories is used to get the categories from the categoryService.
-  // getFiltered has a subscribe method that is used to handle the response from the getFilteredCategories method.
-  // The response is assigned to the categories variable which is used to display the categories in the template.
-  getCategories() {
+  getCategorys() {
     this.categoryService.getFilteredCategories().subscribe((response: any) => {
-      this.ngxService.stop();
-      this.categories = response;
-  }, (error: any) => {
-      this.ngxService.stop();
+      this.categorys = response;
+    }, (error: any) => {
       console.log(error.error?.message);
-      if(error.error?.message) { 
+      if (error.error?.message) {
         this.responseMessage = error.error?.message;
       } else {
         this.responseMessage = GlobalConstants.genericError;
       }
-      this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
-  }
-  );
+      this.SnackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
   }
 
-
-  getProductsByCategoryId(value: any){
+  getProductsByCategory(value: any) {
     this.productService.getProductByCategoryId(value.id).subscribe((response: any) => {
       this.products = response;
       this.manageOrderForm.controls['price'].setValue('');
       this.manageOrderForm.controls['quantity'].setValue('');
       this.manageOrderForm.controls['total'].setValue(0);
     },(error: any) => {
-      console.log(error);
-      if(error.error?.message) { 
+      console.log(error.error?.message);
+      if (error.error?.message) {
         this.responseMessage = error.error?.message;
       } else {
         this.responseMessage = GlobalConstants.genericError;
       }
-      this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
-    }) 
+      this.SnackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
   }
 
-  getProductDetails(value: any){
+
+  getProductDetails(value: any) {
+    //console.log("inside getProductDetails");
     this.productService.getProductById(value.id).subscribe((response: any) => {
       this.price = response.price;
       this.manageOrderForm.controls['price'].setValue(response.price);
       this.manageOrderForm.controls['quantity'].setValue('1');
-      this.manageOrderForm.controls['total'].setValue(this.price*1);
-    },(error: any) => {
-      console.log(error);
-      if(error.error?.message) { 
+      this.manageOrderForm.controls['total'].setValue(this.price * 1);
+    }, (error: any) => {
+      console.log(error.error?.message);
+      if (error.error?.message) {
         this.responseMessage = error.error?.message;
       } else {
         this.responseMessage = GlobalConstants.genericError;
       }
-      this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
-    }) 
+      this.SnackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
   }
 
-  setQuantity(value: any){
+  setQuantity(value: any) {
     var temp = this.manageOrderForm.controls['quantity'].value;
     if (temp > 0) {
       this.manageOrderForm.controls['total'].setValue(this.manageOrderForm.controls['quantity'].value * this.manageOrderForm.controls['price'].value);
-    }  else if(temp !='') {
-      this.manageOrderForm.controls['quantity'].setValue(1);
-      this.manageOrderForm.controls['total'].setValue(this.manageOrderForm.controls['quantity'] * this.manageOrderForm.controls['price'].value);
+    } else if (temp != '') {
+      this.manageOrderForm.controls['quantity'].setValue('1');
+      this.manageOrderForm.controls['total'].setValue(this.manageOrderForm.controls['quantity'].value * this.manageOrderForm.controls['price'].value);
     }
   }
 
-  validateProductAdd(){
-    if(this.manageOrderForm.controls['total'].value === 0 || this.manageOrderForm.controls['total'].value === null || this.manageOrderForm.controls['quantity'].value <= 0){
+  validateProductAdd() {
+    var fromData = this.manageOrderForm.value;
+
+    //var totalValue = this.manageOrderForm.contols['total'].value;
+    var Value = this.manageOrderForm.controls['price'].value;
+    if ( Value === null || fromData?.product?.total === 0 || fromData?.product?.total === '' || fromData?.product?.quantity <= 0) {
       return true;
     } else {
       return false;
     }
   }
 
-  validateSubmit(){
-    if (this.totalAmount === 0 || this.manageOrderForm.controls['name'].value === null || this.manageOrderForm.controls['email'].value === null || 
-      this.manageOrderForm.controls['contactNumber'].value === null || this.manageOrderForm.controls['paymentMethod'].value === null) {
-        // this will disable the submit button
-        return true;
+
+  validateSubmit() {
+    var formData = this.manageOrderForm.value;
+    if (this.totalAmount === 0 || formData.product.name === null || this.manageOrderForm.controls['email'].value === null ||
+    formData.contactNumber === null || formData.paymentMethod === null) {
+      return true;
     } else {
       return false;
     }
   }
 
   add(){
-    var formData = this.manageOrderForm.value;
-    var productName = this.dataSource.find((e:{id:number}) => e.id === formData.product.id);
-    if (productName === undefined){
-      this.totalAmount = this.totalAmount + formData.total;
-      this.dataSource.push({id: formData.product.id, name: formData.product.name, category: formData.category.name, price: formData.price, quantity: formData.quantity, total: formData.total});
+    var fromData = this.manageOrderForm.value;
+    var productName = this.dataSource.find((e:{id:number}) => e.id === fromData.product.id);
+    if(productName === undefined){
+      this.totalAmount = this.totalAmount + fromData.total;
+      this.dataSource.push({id:fromData.product.id , name:fromData.product.name , category:fromData.category.name, quantity:fromData.quantity, price:fromData.price,total:fromData.total});
       this.dataSource = [...this.dataSource];
-      this.snackbarService.openSnackBar(GlobalConstants.productAdded, 'Success');
-    } else {
-      this.snackbarService.openSnackBar(GlobalConstants.productExistError, GlobalConstants.error);
+      //alert("Order Added Successfully");
+      this.SnackbarService.openSnackBar(GlobalConstants.productAdded , "Success");
+    }else{
+      this.SnackbarService.openSnackBar(GlobalConstants.productExistError , GlobalConstants.error);
     }
   }
 
-  handleDeleteAction(value:any, element:any){
-    this.totalAmount = this.totalAmount - element.total;
-    this.dataSource.splice(value, 1);
+  handleDeleteAction(value:any , element:any){
+    this.totalAmount = this.totalAmount = element.total;
+    this.dataSource.splice(value,1);
     this.dataSource = [...this.dataSource];
   }
 
   submitAction(){
     var formData = this.manageOrderForm.value;
     var data = {
-      name: formData.name,
-      email: formData.email,
-      contactNumber: formData.contactNumber,
-      paymentMethod: formData.paymentMethod,
-      totalAmount: this.totalAmount.toString(),
-      productDetails: JSON.stringify(this.dataSource)
+      name : formData.name,
+      email : formData.email,
+      contactNumber : formData.contactNumber,
+      paymentMethod : formData.paymentMethod,
+      totalAmount : this.totalAmount.toString(),
+      productDetails : JSON.stringify(this.dataSource)
     }
-    this.ngxService.start();
-    this.billService.generateReport(data).subscribe((response: any) => {
-      this.downloadFile(response?.uuid);
+
+    this.billService.generateReport(data).subscribe((resonse:any)=>{
+      this.downloadFile(resonse?.uuid);
       this.manageOrderForm.reset();
       this.dataSource = [];
       this.totalAmount = 0;
-    },(error: any) => {
-      console.log(error.error?.message);
-      if(error.error?.message) { 
-        this.responseMessage = error.error?.message;
-      } else {
-        this.responseMessage = GlobalConstants.genericError;
-      }
-      this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
-    })
+      },(error: any) => {
+        console.log(error.error?.message);
+        if (error.error?.message) {
+          this.responseMessage = error.error?.message;
+        } else {
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.SnackbarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+      })
   }
-
-  downloadFile(fileName: string) {
+  downloadFile(fileName:string) {
     var data = {
-      uuid: fileName
+      uuid : fileName
     }
-    this.billService.getPdf(data).subscribe((response: any) => {
-      saveAs(response, fileName + '.pdf');
-      this.ngxService.stop();
+    this.billService.getPdf(data).subscribe((resonse:any)=>{
+        saveAs(resonse , fileName +".pdf");
     })
   }
-
 }
